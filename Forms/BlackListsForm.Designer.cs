8e8a1ef2d5d15ddcdc19b09d1b462acb9bfb2836
Kilka Danil <kritik.roro@mail.ru>
2024-05-15 17:13:18 +0300

    Баг с регистрацией

--- Forms/BlackListsForm.Designer.cs
+++ Forms/BlackListsForm.Designer.cs
@@ -29,15 +29,23 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BlackList));
-            BlackListButton = new Button();
             Picture6 = new PictureBox();
+            BlackListButton = new Button();
+            tableLayoutPanel1 = new TableLayoutPanel();
             listView1 = new ListView();
             PhotoList = new ColumnHeader();
             PriceList = new ColumnHeader();
             AddressList = new ColumnHeader();
             NameList = new ColumnHeader();
             ((System.ComponentModel.ISupportInitialize)Picture6).BeginInit();
+            tableLayoutPanel1.SuspendLayout();
             SuspendLayout();
+            // 
+            // Picture6
+            // 
+            resources.ApplyResources(Picture6, "Picture6");
+            Picture6.Name = "Picture6";
+            Picture6.TabStop = false;
             // 
             // BlackListButton
             // 
@@ -47,11 +55,12 @@
             BlackListButton.UseVisualStyleBackColor = false;
             BlackListButton.Click += BlackListButton_Click;
             // 
-            // Picture6
+            // tableLayoutPanel1
             // 
-            resources.ApplyResources(Picture6, "Picture6");
-            Picture6.Name = "Picture6";
-            Picture6.TabStop = false;
+            resources.ApplyResources(tableLayoutPanel1, "tableLayoutPanel1");
+            tableLayoutPanel1.Controls.Add(listView1, 0, 0);
+            tableLayoutPanel1.Controls.Add(BlackListButton, 0, 1);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
             // 
             // listView1
             // 
@@ -82,19 +91,21 @@
             // 
             resources.ApplyResources(this, "$this");
             AutoScaleMode = AutoScaleMode.Font;
-            Controls.Add(listView1);
-            Controls.Add(BlackListButton);
+            Controls.Add(tableLayoutPanel1);
             Controls.Add(Picture6);
             FormBorderStyle = FormBorderStyle.SizableToolWindow;
             Name = "BlackList";
             Load += BlackList_Load;
+            SizeChanged += BlackList_SizeChanged;
             ((System.ComponentModel.ISupportInitialize)Picture6).EndInit();
+            tableLayoutPanel1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
-        private System.Windows.Forms.Button BlackListButton;
         private System.Windows.Forms.PictureBox Picture6;
+        private Button BlackListButton;
+        private TableLayoutPanel tableLayoutPanel1;
         private ListView listView1;
         private ColumnHeader PhotoList;
         private ColumnHeader PriceList;
--- Forms/BlackListsForm.cs
+++ Forms/BlackListsForm.cs
@@ -2,28 +2,51 @@
 using DB_993.Resourse;
 
 namespace design
-{
+{/// <summary>
+ /// Класс используется для добавления объектов в черный список, отображения информации об объектах и удаления объекта из черного списка.
+ /// </summary>
     public partial class BlackList : Form
     {
-        /// <summary>
-        /// Класс используется для добавления объектов в черный список, отображения информации об объектах и удаления объекта из черного списка.
-        /// </summary>
+        public int Column1 { get; set; }
+        public int Column2 { get; set; }
+        public int Column3 { get; set; }
+        public int Column4 { get; set; }
+        public int HeightLoc { get; set; }
+        public int WidthLoc { get; set; }
+        public int HeightF { get; set; }
+        public int WidthF { get; set; }
         public int IdRealryForFav { get; set; }
         public string? Email { get; set; }
         public BlackList()
         {
             InitializeComponent();
             LoadData();
-
+            Design();
         }
         public BlackList(int idRealty)
         {
             IdRealryForFav = idRealty;
             FillTableFavourites();
             InitializeComponent();
-
+            Design();
         }
-
+        public void Design()
+        {
+            Column1 = listView1.Columns[0].Width;
+            Column2 = listView1.Columns[1].Width;
+            Column3 = listView1.Columns[2].Width;
+            Column4 = listView1.Columns[3].Width;
+            listView1.Anchor = AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Top | AnchorStyles.Bottom;
+            BlackListButton.Anchor = AnchorStyles.Left;
+            tableLayoutPanel1.BackColor = Color.Transparent;
+            tableLayoutPanel1.Width = 769;
+            WidthF = this.Width;
+            HeightF = this.Height;
+            tableLayoutPanel1.Height = 740;
+            HeightLoc = 740;
+            WidthLoc = 769;
+            Picture6.Anchor = AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Top | AnchorStyles.Bottom;
+        }
 
         private void FillTableFavourites()
         {
@@ -106,6 +129,22 @@ namespace design
         {
             var t = new ToolTip();
             t.SetToolTip(BlackListButton, BlackListLocal.BlackListClearText);
+        }
+
+        private void BlackList_SizeChanged(object sender, EventArgs e)
+        {
+            tableLayoutPanel1.Width = (int)(((float)WidthLoc * ((float)this.Width / (float)WidthF)));
+            tableLayoutPanel1.Height = (int)(((float)HeightLoc * ((float)this.Height / (float)HeightF)));
+
+
+            listView1.Columns[0].Width = (int)(Column1 * ((float)tableLayoutPanel1.Width / (float)WidthLoc));
+            listView1.Columns[1].Width = (int)(Column2 * ((float)tableLayoutPanel1.Width / (float)WidthLoc));
+            listView1.Columns[2].Width = (int)(Column3 * ((float)tableLayoutPanel1.Width / (float)WidthLoc));
+            listView1.Columns[3].Width = (int)(Column4 * ((float)tableLayoutPanel1.Width / (float)WidthLoc));
+
+            int newX = (this.ClientSize.Width - tableLayoutPanel1.Width) / 2;
+            int newY = (this.ClientSize.Height - tableLayoutPanel1.Height) / 2;
+            tableLayoutPanel1.Location = new Point(newX, newY);
         }
     }
 }
